aad6ce628f1726bc9b742067bea3432d19a0e0e8
Zapata Ulises <zapataulises01@outlook.com.ar>
2022-10-10 21:09:17 -0300

    burgerFix

--- Segunda Entrega/js/main.js
+++ Segunda Entrega/js/main.js
@@ -56,5 +56,28 @@ document.addEventListener("DOMContentLoaded", function () {
 })
 })
 
+let burger=0;
+let burgerMenu=document.querySelector("#menuVertical");
+document.querySelector("#burger").addEventListener("click", function () {
+    
+    if (burger==0) {
+
+        burgerMenu.classList.add("menu-on");
+        burgerMenu.classList.remove("menu-off");
+        burger=1;
+    }
+    else if (burger==1) {
+        burgerMenu.classList.remove("menu-on");
+        burgerMenu.classList.add("menu-off");
+        burger=0;
+    }
+})
+    
+
+    
+    
+
+
+
 });
 
