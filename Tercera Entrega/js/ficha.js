deb135863c5179db239d122cbc84864226c9781a
Aaron Abella <aaronabella1382@gmail.com>
2022-11-01 20:48:33 -0300

    constructor clase

--- Tercera Entrega/js/ficha.js
+++ Tercera Entrega/js/ficha.js
@@ -1,4 +1,23 @@
 "use strict";
 
 class Ficha  {
+    posx;
+    posy;
+    jugador;
+    seleccionada;
+    turno;
+    
+    constructor(x,y,jugador,seleccionada){
+        this.posx= posx;
+        this.posy= posy;
+        this.jugador= jugador;
+        this.seleccionada= seleccionada;
+        this.turno = false;
+    }
+    getPosx(){
+        return this.posx;
+    }
+    getPosY{
+        
+    }
 }
