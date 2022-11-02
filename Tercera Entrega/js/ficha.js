cd2a285a48c1b11178a2915f41702b5365f5dd9a
Zapata Ulises <zapataulises01@outlook.com.ar>
2022-11-01 22:09:29 -0300

    Update ficha.js

--- Tercera Entrega/js/ficha.js
+++ Tercera Entrega/js/ficha.js
@@ -6,18 +6,33 @@ class Ficha  {
     jugador;
     seleccionada;
     turno;
+    startx;
+    starty;
     
     constructor(x,y,jugador,seleccionada){
-        this.posx= posx;
-        this.posy= posy;
+        this.posx= x;
+        this.posy= y;
+        this.startx= x;
+        this.starty= y;
         this.jugador= jugador;
         this.seleccionada= seleccionada;
         this.turno = false;
+    }
+    toStart(){
+        this.posx=this.startx;
+        this.posy=this.starty;
     }
     getPosx(){
         return this.posx;
     }
     getPosY(){
         return this.posy;
+    }
+
+    draw(){
+        let img= this.jugador.img
+        x = this.posx;
+        y = this.posy;
+        ctx.drawImage(img, x, y, 45, 45);
     }
 }
