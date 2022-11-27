12f73fafdc5ff5ad25af4eb75e265a9efac654fa
Zapata Ulises <zapataulises01@outlook.com.ar>
2022-11-27 16:09:37 -0300

    animado ahi

--- Cuarta Entrega/js/hero.js
+++ Cuarta Entrega/js/hero.js
@@ -6,26 +6,28 @@ document.addEventListener("DOMContentLoaded", function () {
     let planetas= document.querySelectorAll(".planeta")
     let i=0
     flechaDer.addEventListener("click", function () {
-       
         planetas[i].classList.add("off-plan");
-        planetas[i].classList.remove("on-plan"); 
+        planetas[i].classList.remove("on-plan-izq")
+        planetas[i].classList.remove("on-plan-der"); 
         if (i==planetas.length-1) {
             i=-1
         }
         planetas[i+1].classList.remove("off-plan");
-        planetas[i+1].classList.add("on-plan");
+        planetas[i+1].classList.add("on-plan-der");
+   
         i++;
 
     })
     flechaIzq.addEventListener("click", function () {
        
         planetas[i].classList.add("off-plan");
-        planetas[i].classList.remove("on-plan"); 
+        planetas[i].classList.remove("on-plan-izq"); 
+        planetas[i].classList.remove("on-plan-der")
         if (i==0) {
             i=planetas.length;
         }
         planetas[i-1].classList.remove("off-plan");
-        planetas[i-1].classList.add("on-plan");
+        planetas[i-1].classList.add("on-plan-izq");
         i--;
 
     })
