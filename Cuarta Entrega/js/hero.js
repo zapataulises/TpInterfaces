5678013fd92de0efa4700a6e6aae35715dc71f63
Zapata Ulises <zapataulises01@outlook.com.ar>
2022-11-28 18:33:28 -0300

    Animacion Titulo God

--- Cuarta Entrega/js/hero.js
+++ Cuarta Entrega/js/hero.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     var flechaDer= document.getElementById("f-der");
     let planetas= document.querySelectorAll(".planeta")
     var navbar = document.getElementById("navbarh");
-    var titulo = document.querySelector(".probando");
+    
     let i=0
     flechaDer.addEventListener("click", async function () {
         planetas[i].classList.add("off-plan-izq");
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
         planetas[i].classList.add("off-plan-der");
         planetas[i].classList.remove("on-plan-izq"); 
         planetas[i].classList.remove("on-plan-der")
-        await(delay(500));
+        await(delay(10));
         planetas[i].classList.remove("off-plan-der");
         planetas[i].classList.add("off-plan");
         if (i==0) {
@@ -42,41 +42,65 @@ document.addEventListener("DOMContentLoaded", function () {
         return new Promise(resolve => setTimeout(resolve, time));
       }
 
-window.addEventListener("scroll", function () {
+window.addEventListener("scroll", async function () {
     let textos = document.querySelectorAll(".texto");
-    let imgs = document.querySelectorAll(".img")
+    let imgs = document.querySelectorAll(".img");
+    let titulo = document.querySelector(".probando");
+    
     textos.forEach(function (t) {
         t.classList.add("oculto")
       })
       imgs.forEach(function (t) {
         t.classList.add("imgOculta")
       })
-      if(scrollY > 30){
-        titulo.classList.add("efecto");
+
+      titulo.style.opacity
+      if(scrollY<100){
+        titulo.style.opacity= 1;
+        titulo.style.top= '210px';
       }
-      if (scrollY < 1200) {
+      else if(scrollY >= 100 && scrollY < 140){
+        titulo.style.opacity=0.8;
+        titulo.style.top= '260px';
+      }
+      else if(scrollY >= 140 && scrollY <190){
+        titulo.style.opacity=0.6;
+        titulo.style.top= '310px';
+      }
+      else if(scrollY >= 190 && scrollY < 220){
+        titulo.style.opacity=0.4;
+        titulo.style.top= '360px';
+      }
+      else if(scrollY >= 220 && scrollY < 350){
+        titulo.style.opacity=0.2;
+        titulo.style.top= '410px';
+      }
+      else if(scrollY >= 350){
+        titulo.style.top= '460px';
+        titulo.style.opacity=0;
+      }
+      if (scrollY >= 800 && scrollY < 1200) {
         textos[0].classList.remove("oculto");
         imgs[0].classList.remove("imgOculta");
-        navbar.classList.add("achicar");
       }
       else if (scrollY >= 1200 && scrollY < 1700) {
         textos[1].classList.remove("oculto");
         imgs[1].classList.remove("imgOculta");
-        navbar.classList.add("achicar");
-        navbar.classList.remove("desachicar")
+       // navbar.classList.add("achicar");
+        //navbar.classList.remove("desachicar")
       } 
       
     
       else {
         textos[2].classList.remove("oculto")
         imgs[2].classList.remove("imgOculta");
-        navbar.classLista.remove("achicar");
-        navbar.classList.add("desachicar");
+        titulo.classList.remove("oculto");
+        //navbar.classLista.remove("achicar");
+        //navbar.classList.add("desachicar");
       }
     })
     
-    
-
+  //navbar.classList.add("achicar");
 })
 
     
