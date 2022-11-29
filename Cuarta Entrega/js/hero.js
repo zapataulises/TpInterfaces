0483d02426f9dca081a49dc039a70d4255859abc
Aaron Abella <aaronabella1382@gmail.com>
2022-11-28 21:10:23 -0300

    Fixed navbar sitcky

--- Cuarta Entrega/js/hero.js
+++ Cuarta Entrega/js/hero.js
@@ -64,7 +64,6 @@ window.addEventListener("scroll", async function () {
       if(scrollY<100 && scrollY>1){
         titulo.style.opacity= 1;
         titulo.style.top= '210px';
-        navbar.style.transform="scale(0.5)"
         icon.style.height="1.2em";
         icon.style.width="1.2em";
         navbar.style.top='0'
